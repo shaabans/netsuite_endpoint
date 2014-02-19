a71f27030d6b629258781a477b931dcf728a2af5
Andrei Bondarev <andrei@spreecommerce.com>
2014-02-19 00:07:46 -0500

    Refund class -> v6

--- lib/netsuite_integration/refund.rb
+++ lib/netsuite_integration/refund.rb
@@ -1,20 +1,17 @@
 module NetsuiteIntegration
   class Refund < Base
-    attr_reader :config, :user_id, :original_payload, :order_payload, :sales_order, :customer_deposit, :customer
+    attr_reader :config, :order_payload, :sales_order, :customer_deposit, :customer
 
     def initialize(config, message, order)
       super(message, config)
-
+      
       @config           = config
-      @user_id          = original[:user_id]
-      @order_payload    = payload[:order]
-      @original_payload = payload[:original]
-
+      @order_payload    = message
       @sales_order      = order
       @customer_deposit = customer_deposit_service.find_by_external_id(order_payload[:number]) or
         raise RecordNotFoundCustomerDeposit, "NetSuite Customer Deposit not found for order #{order_payload[:number]}"
       @customer = customer_service.find_by_external_id(order_payload[:email]) or
-        raise RecordNotFoundCustomerException, "NetSuite Customer not found for Spree user_id #{user_id}"
+        raise RecordNotFoundCustomerException, "NetSuite Customer not found for Spree User #{order_payload[:email]}"
     end
 
     def process!
@@ -25,7 +22,7 @@ module NetsuiteIntegration
 
     private
     def payment_method_id
-      method = @payload[:order][:payments][0][:payment_method]
+      method = order_payload[:payments][0][:payment_method]
       @config['netsuite.payment_methods_mapping'][0].fetch(method).to_i
     rescue
       raise "Payment method #{method} not found in #{@config['netsuite.payment_methods_mapping'].inspect}"
